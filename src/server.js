46aeac6fee64b6ff86e96a18517b5e9c58c7328e
ownue <enu_i@naver.com>
2026-05-21 15:46:23 +0900

    feat: sync 추가

--- src/server.js
+++ src/server.js
@@ -2,9 +2,29 @@
 require("dotenv").config();
 
 const app = require("./app");
+const { sequelize } = require("../models");
 
 const PORT = process.env.PORT || 8082;
 
-app.listen(PORT, () => {
-  console.log(`schedule-svc running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await sequelize.authenticate();
+
+        console.log("DB connected");
+
+        await sequelize.sync({
+            alter: true,
+        });
+
+        console.log("DB sync completed");
+
+        app.listen(PORT, () => {
+            console.log(`schedule-svc listening on ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+startServer();
