ccb3b55a8906ddeb0ca3917c4addff4828d32e97
ownue <enu_i@naver.com>
2026-05-26 13:27:24 +0900

    refactor: 중복 index 삭제

--- models/region.js
+++ models/region.js
@@ -13,13 +13,19 @@ module.exports = (sequelize, DataTypes) => {
             region_name: {
                 type: DataTypes.STRING(100),
                 allowNull: false,
-                unique: true,
             },
         },
         {
             tableName: "regions",
             underscored: true,
             timestamps: true,
+            indexes: [
+                {
+                    unique: true,
+                    fields: ["region_name"],
+                    name: "uk_regions_region_name",
+                },
+            ],
         },
     );
 
