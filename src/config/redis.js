88d8e2d890b2dd826d3c2bd818fc63c994f82ab8
ownue <enu_i@naver.com>
2026-05-27 12:35:17 +0900

    chore: redis 연결 코드 안전하게 수정

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,11 @@
 // schedule-svc/src/config/redis.js
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
+const redisUrl =
+    process.env.REDIS_URL ||
+    `redis://${process.env.REDIS_HOST || "localhost"}:${process.env.REDIS_PORT || 6379}`;
+
+const redis = new Redis(redisUrl, {
     maxRetriesPerRequest: 3,
     enableReadyCheck: true,
 });
